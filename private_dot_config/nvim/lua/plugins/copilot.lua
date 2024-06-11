c8d1887ea7fae784900eadd6b85fb9c91011910f
YDH <swimmingpolar@gmail.com>
2024-06-11 19:13:38 +0900

    Update .config/nvim/init.lua Update .config/nvim/lazy-lock.json Update .config/nvim/lazyvim.json Add .config/nvim/lua/colors/gruvbox-material.lua Add .config/nvim/lua/colors/gruvluke/colors/gruvluke.lua Add .config/nvim/lua/colors/gruvluke/gruvluke/groups.lua Add .config/nvim/lua/colors/gruvluke/gruvluke/init.lua Add .config/nvim/lua/colors/gruvluke/gruvluke/palette.lua Add .config/nvim/lua/config/lazy.lua Update .config/nvim/lua/config/options.lua Update .config/nvim/lua/plugins/cmp.lua Update .config/nvim/lua/plugins/copilot.lua Update .config/nvim/stylua.toml

--- private_dot_config/nvim/lua/plugins/copilot.lua
+++ private_dot_config/nvim/lua/plugins/copilot.lua
@@ -1,43 +1,19 @@
 return {
-	'zbirenbaum/copilot.lua',
-	cmd = 'Copilot',
-	event = 'InsertEnter',
-	build = ':Copilot auth',
-	config = function(_, opts)
-		require('copilot').setup(opts)
-		local suggestion = lazy_require 'copilot.suggestion'
-		vim.keymap.set('i', '<Esc>', function()
-			if suggestion.is_visible() then
-				suggestion.dismiss()
-			end
-			return '<Esc>'
-		end, {
-			desc = '[copilot] dismiss suggestion',
-			expr = true,
-			silent = true,
-		})
-	end,
-	opts = {
-		panel = { enabled = false },
-		suggestion = {
-			enabled = true,
-			auto_trigger = true,
-			keymap = {
-				accept = false, -- handled by cmp
-				dismiss = false,
-				accept_word = false,
-				accept_line = false,
-				next = false,
-				prev = false,
-			},
-		},
-		filetypes = { ['*'] = true },
-		server_opts_overrides = {
-			settings = {
-				advanced = {
-					inlineSuggestCount = 1,
-				},
-			},
-		},
-	},
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+      require("copilot").setup({
+        suggestion = { enabled = false },
+        panel = { enabled = false },
+      })
+    end,
+  },
+  {
+    "zbirenbaum/copilot-cmp",
+    config = function()
+      require("copilot_cmp").setup()
+    end,
+  },
 }
