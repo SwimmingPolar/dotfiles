379ca389cbde15c316e4246c940e97186ea9074b
YDH <swimmingpolar@gmail.com>
2024-07-07 14:26:23 +0900

    Add .config/nvim/LICENSE Add .config/nvim/README.md Add .config/nvim/.git/HEAD Add .config/nvim/.git/branches/.keep Add .config/nvim/.git/config Add .config/nvim/.git/description Add .config/nvim/.git/hooks/applypatch-msg.sample Add .config/nvim/.git/hooks/commit-msg.sample Add .config/nvim/.git/hooks/fsmonitor-watchman.sample Add .config/nvim/.git/hooks/post-update.sample Add .config/nvim/.git/hooks/pre-applypatch.sample Add .config/nvim/.git/hooks/pre-commit.sample Add .config/nvim/.git/hooks/pre-merge-commit.sample Add .config/nvim/.git/hooks/pre-push.sample Add .config/nvim/.git/hooks/pre-rebase.sample Add .config/nvim/.git/hooks/pre-receive.sample Add .config/nvim/.git/hooks/prepare-commit-msg.sample Add .config/nvim/.git/hooks/push-to-checkout.sample Add .config/nvim/.git/hooks/update.sample Add .config/nvim/.git/index Add .config/nvim/.git/info/exclude Add .config/nvim/.git/logs/HEAD Add .config/nvim/.git/logs/refs/heads/main Add .config/nvim/.git/logs/refs/remotes/origin/HEAD Add .config/nvim/.git/objects/info/.keep Add .config/nvim/.git/objects/pack/pack-86ff5b2920c950d77cf1b5dcab63824903640d0f.idx Add .config/nvim/.git/objects/pack/pack-86ff5b2920c950d77cf1b5dcab63824903640d0f.pack Add .config/nvim/.git/packed-refs Add .config/nvim/.git/refs/heads/main Add .config/nvim/.git/refs/remotes/origin/HEAD Add .config/nvim/.git/refs/tags/.keep Add .config/nvim/.gitignore Add .config/nvim/.neoconf.json Add .config/nvim/init.lua Add .config/nvim/lazy-lock.json Add .config/nvim/lazyvim.json Add .config/nvim/lua/config/autocmds.lua Add .config/nvim/lua/config/keymaps.lua Add .config/nvim/lua/config/lazy.lua Add .config/nvim/lua/config/options.lua Add .config/nvim/lua/plugins/coding/format.lua Add .config/nvim/lua/plugins/example.lua Add .config/nvim/lua/plugins/format/.keep Add .config/nvim/lua/plugins/ui/color.lua Add .config/nvim/lua/plugins/ui/neotree.lua Add .config/nvim/lua/plugins/utils/enhanced-fzf.lua Add .config/nvim/lua/plugins/utils/supertab.lua Add .config/nvim/stylua.toml

---
+++ dot_config/nvim/lua/config/options.lua
@@ -0,0 +1,73 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+-- General
+vim.opt.number = true -- Show line numbers
+vim.opt.wrap = false -- No line wrap
+vim.opt.showbreak = "+++" -- Wrap-broken line prefix
+vim.opt.textwidth = 100 -- Line wrap (number of cols)
+vim.opt.showmatch = true -- Highlight matching brace
+vim.opt.visualbell = true -- Use visual bell (no beeping)
+
+vim.opt.hlsearch = true -- Highlight all search results
+vim.opt.smartcase = true -- Enable smart-case search
+vim.opt.ignorecase = true -- Always case-insensitive
+vim.opt.incsearch = true -- Searches for strings incrementally
+
+vim.opt.autoindent = true -- Auto-indent new lines
+vim.opt.cindent = true -- Use 'C' style program indenting
+vim.opt.expandtab = true -- Use spaces instead of tabs
+vim.opt.shiftwidth = 2 -- Number of auto-indent spaces
+vim.opt.smartindent = true -- Enable smart-indent
+vim.opt.smarttab = true -- Enable smart-tabs
+vim.opt.softtabstop = 2 -- Number of spaces per Tab
+
+-- Advanced
+vim.opt.undolevels = 1000 -- Number of undo levels
+vim.opt.backspace = "indent,eol,start" -- Backspace behaviour
+
+-- Completion
+vim.opt.completeopt = { "menu", "menuone", "noselect" }
+
+-- Customization for Pmenu
+vim.api.nvim_set_hl(0, "PmenuSel", { bg = "#282C34", fg = "NONE" })
+vim.api.nvim_set_hl(0, "Pmenu", { fg = "#C5CDD9", bg = "#22252A" })
+
+vim.api.nvim_set_hl(0, "CmpItemAbbrDeprecated", { fg = "#7E8294", bg = "NONE", strikethrough = true })
+vim.api.nvim_set_hl(0, "CmpItemAbbrMatch", { fg = "#82AAFF", bg = "NONE", bold = true })
+vim.api.nvim_set_hl(0, "CmpItemAbbrMatchFuzzy", { fg = "#82AAFF", bg = "NONE", bold = true })
+vim.api.nvim_set_hl(0, "CmpItemMenu", { fg = "#C792EA", bg = "NONE", italic = true })
+
+vim.api.nvim_set_hl(0, "CmpItemKindField", { fg = "#EED8DA", bg = "#B5585F" })
+vim.api.nvim_set_hl(0, "CmpItemKindProperty", { fg = "#EED8DA", bg = "#B5585F" })
+vim.api.nvim_set_hl(0, "CmpItemKindEvent", { fg = "#EED8DA", bg = "#B5585F" })
+
+vim.api.nvim_set_hl(0, "CmpItemKindText", { fg = "#C3E88D", bg = "#9FBD73" })
+vim.api.nvim_set_hl(0, "CmpItemKindEnum", { fg = "#C3E88D", bg = "#9FBD73" })
+vim.api.nvim_set_hl(0, "CmpItemKindKeyword", { fg = "#C3E88D", bg = "#9FBD73" })
+
+vim.api.nvim_set_hl(0, "CmpItemKindConstant", { fg = "#FFE082", bg = "#D4BB6C" })
+vim.api.nvim_set_hl(0, "CmpItemKindConstructor", { fg = "#FFE082", bg = "#D4BB6C" })
+vim.api.nvim_set_hl(0, "CmpItemKindReference", { fg = "#FFE082", bg = "#D4BB6C" })
+
+vim.api.nvim_set_hl(0, "CmpItemKindFunction", { fg = "#EADFF0", bg = "#A377BF" })
+vim.api.nvim_set_hl(0, "CmpItemKindStruct", { fg = "#EADFF0", bg = "#A377BF" })
+vim.api.nvim_set_hl(0, "CmpItemKindClass", { fg = "#EADFF0", bg = "#A377BF" })
+vim.api.nvim_set_hl(0, "CmpItemKindModule", { fg = "#EADFF0", bg = "#A377BF" })
+vim.api.nvim_set_hl(0, "CmpItemKindOperator", { fg = "#EADFF0", bg = "#A377BF" })
+
+vim.api.nvim_set_hl(0, "CmpItemKindVariable", { fg = "#C5CDD9", bg = "#7E8294" })
+vim.api.nvim_set_hl(0, "CmpItemKindFile", { fg = "#C5CDD9", bg = "#7E8294" })
+
+vim.api.nvim_set_hl(0, "CmpItemKindUnit", { fg = "#F5EBD9", bg = "#D4A959" })
+vim.api.nvim_set_hl(0, "CmpItemKindSnippet", { fg = "#F5EBD9", bg = "#D4A959" })
+vim.api.nvim_set_hl(0, "CmpItemKindFolder", { fg = "#F5EBD9", bg = "#D4A959" })
+
+vim.api.nvim_set_hl(0, "CmpItemKindMethod", { fg = "#DDE5F5", bg = "#6C8ED4" })
+vim.api.nvim_set_hl(0, "CmpItemKindValue", { fg = "#DDE5F5", bg = "#6C8ED4" })
+vim.api.nvim_set_hl(0, "CmpItemKindEnumMember", { fg = "#DDE5F5", bg = "#6C8ED4" })
+
+vim.api.nvim_set_hl(0, "CmpItemKindInterface", { fg = "#D8EEEB", bg = "#58B5A8" })
+vim.api.nvim_set_hl(0, "CmpItemKindColor", { fg = "#D8EEEB", bg = "#58B5A8" })
+vim.api.nvim_set_hl(0, "CmpItemKindTypeParameter", { fg = "#D8EEEB", bg = "#58B5A8" })
