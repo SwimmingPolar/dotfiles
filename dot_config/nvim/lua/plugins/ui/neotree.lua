aad82e2f4e574ed3a386ca9005331a7b72492f60
YDH <swimming.polar@gmail.com>
2024-07-18 11:42:01 +0900

    Add .config/nvim/LICENSE Add .config/nvim/README.md Add .config/nvim/.git/HEAD Add .config/nvim/.git/branches/.keep Add .config/nvim/.git/config Add .config/nvim/.git/description Add .config/nvim/.git/hooks/applypatch-msg.sample Add .config/nvim/.git/hooks/commit-msg.sample Add .config/nvim/.git/hooks/fsmonitor-watchman.sample Add .config/nvim/.git/hooks/post-update.sample Add .config/nvim/.git/hooks/pre-applypatch.sample Add .config/nvim/.git/hooks/pre-commit.sample Add .config/nvim/.git/hooks/pre-merge-commit.sample Add .config/nvim/.git/hooks/pre-push.sample Add .config/nvim/.git/hooks/pre-rebase.sample Add .config/nvim/.git/hooks/pre-receive.sample Add .config/nvim/.git/hooks/prepare-commit-msg.sample Add .config/nvim/.git/hooks/push-to-checkout.sample Add .config/nvim/.git/hooks/update.sample Add .config/nvim/.git/index Add .config/nvim/.git/info/exclude Add .config/nvim/.git/logs/HEAD Add .config/nvim/.git/logs/refs/heads/main Add .config/nvim/.git/logs/refs/remotes/origin/HEAD Add .config/nvim/.git/objects/info/.keep Add .config/nvim/.git/objects/pack/pack-86ff5b2920c950d77cf1b5dcab63824903640d0f.idx Add .config/nvim/.git/objects/pack/pack-86ff5b2920c950d77cf1b5dcab63824903640d0f.pack Add .config/nvim/.git/packed-refs Add .config/nvim/.git/refs/heads/main Add .config/nvim/.git/refs/remotes/origin/HEAD Add .config/nvim/.git/refs/tags/.keep Add .config/nvim/.gitignore Add .config/nvim/.neoconf.json Add .config/nvim/init.lua Add .config/nvim/lazy-lock.json Add .config/nvim/lazyvim.json Add .config/nvim/lua/config/autocmds.lua Add .config/nvim/lua/config/keymaps.lua Add .config/nvim/lua/config/lazy.lua Add .config/nvim/lua/config/options.lua Add .config/nvim/lua/plugins/coding/copilot.lua Add .config/nvim/lua/plugins/coding/format.lua Add .config/nvim/lua/plugins/ui/bufferline.lua Add .config/nvim/lua/plugins/ui/color.lua Add .config/nvim/lua/plugins/ui/mini-map.lua Add .config/nvim/lua/plugins/ui/neotree.lua Add .config/nvim/lua/plugins/ui/window-picker.lua Add .config/nvim/lua/plugins/utils/enhanced-fzf.lua Add .config/nvim/lua/plugins/utils/supertab.lua Add .config/nvim/stylua.toml

---
+++ dot_config/nvim/lua/plugins/ui/neotree.lua
@@ -0,0 +1,23 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    dependency = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons",
+      "MunifTanjim/nui.nvim",
+    },
+    window = {
+      width = 35,
+    },
+    buffers = {
+      follow_current_file = {
+        enabled = true, -- This will find and focus the file in the active buffer every time
+        --              -- the current file is changed while the tree is open.
+        leave_dirs_open = true, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+      },
+    },
+    filesystem = {
+      bind_to_cwd = true,
+    },
+  },
+}
