e8e1f79582428f577a41dd05cebf2582b686b67f
YDH <swimming.polar@gmail.com>
2024-08-09 16:56:29 +0900

    Add .config/nvim/LICENSE Add .config/nvim/README.md Add .config/nvim/.git/HEAD Add .config/nvim/.git/branches/.keep Add .config/nvim/.git/config Add .config/nvim/.git/description Add .config/nvim/.git/hooks/applypatch-msg.sample Add .config/nvim/.git/hooks/commit-msg.sample Add .config/nvim/.git/hooks/fsmonitor-watchman.sample Add .config/nvim/.git/hooks/post-update.sample Add .config/nvim/.git/hooks/pre-applypatch.sample Add .config/nvim/.git/hooks/pre-commit.sample Add .config/nvim/.git/hooks/pre-merge-commit.sample Add .config/nvim/.git/hooks/pre-push.sample Add .config/nvim/.git/hooks/pre-rebase.sample Add .config/nvim/.git/hooks/pre-receive.sample Add .config/nvim/.git/hooks/prepare-commit-msg.sample Add .config/nvim/.git/hooks/push-to-checkout.sample Add .config/nvim/.git/hooks/update.sample Add .config/nvim/.git/index Add .config/nvim/.git/info/exclude Add .config/nvim/.git/logs/HEAD Add .config/nvim/.git/logs/refs/heads/main Add .config/nvim/.git/logs/refs/remotes/origin/HEAD Add .config/nvim/.git/objects/info/.keep Add .config/nvim/.git/objects/pack/pack-86ff5b2920c950d77cf1b5dcab63824903640d0f.idx Add .config/nvim/.git/objects/pack/pack-86ff5b2920c950d77cf1b5dcab63824903640d0f.pack Add .config/nvim/.git/packed-refs Add .config/nvim/.git/refs/heads/main Add .config/nvim/.git/refs/remotes/origin/HEAD Add .config/nvim/.git/refs/tags/.keep Add .config/nvim/.gitignore Add .config/nvim/.neoconf.json Add .config/nvim/init.lua Add .config/nvim/lazy-lock.json Add .config/nvim/lazyvim.json Add .config/nvim/lua/config/autocmds.lua Add .config/nvim/lua/config/keymaps.lua Add .config/nvim/lua/config/lazy.lua Add .config/nvim/lua/config/options.lua Add .config/nvim/lua/plugins/coding/cmp.lua Add .config/nvim/lua/plugins/coding/console-log.lua Add .config/nvim/lua/plugins/coding/copilot.lua Add .config/nvim/lua/plugins/coding/emmet.lua Add .config/nvim/lua/plugins/coding/multicursor.lua Add .config/nvim/lua/plugins/ui/action-preview.lua Add .config/nvim/lua/plugins/ui/bufferline.lua Add .config/nvim/lua/plugins/ui/color.lua Add .config/nvim/lua/plugins/ui/neo-tree.lua Add .config/nvim/lua/plugins/ui/smart-splits.lua Add .config/nvim/lua/plugins/ui/window-picker.lua Add .config/nvim/lua/plugins/utils/enhanced-fzf.lua Add .config/nvim/lua/plugins/utils/tailwind-tools.lua Add .config/nvim/stylua.toml

---
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,41 @@
+-- Easy scape from vim
+vim.cmd([[
+fun! SetupCommandAlias(from, to)
+	exec 'cnoreabbrev <expr> '.a:from
+		\ .' ((getcmdtype() is# ":" && getcmdline() is# "'.a:from.'")'
+		\ .'? ("'.a:to.'") : ("'.a:from.'"))'
+endfun
+call SetupCommandAlias("W","w")
+call SetupCommandAlias("Q","q")
+call SetupCommandAlias("A","a")
+]])
+
+-- Remap C-x to close buffer
+vim.keymap.set("n", "<C-x>", LazyVim.ui.bufremove, { desc = "Close current buffer" })
+
+-- Move current line / block with Alt-j/k ala vscode.
+vim.keymap.set("n", "<A-j>", ":m .+1<CR>==", { noremap = true, silent = true })
+vim.keymap.set("n", "<A-k>", ":m .-2<CR>==", { noremap = true, silent = true })
+vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
+vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv", { noremap = true, silent = true })
+
+-- Smart-Splits
+-- recommended mappings
+-- resizing splits
+-- these keymaps will also accept a range,
+-- for example `10<A-h>` will `resize_left` by `(10 * config.default_amount)`
+vim.keymap.set("n", "<C-A-h>", require("smart-splits").resize_left)
+vim.keymap.set("n", "<C-A-j>", require("smart-splits").resize_down)
+vim.keymap.set("n", "<C-A-k>", require("smart-splits").resize_up)
+vim.keymap.set("n", "<C-A-l>", require("smart-splits").resize_right)
+-- moving between splits
+vim.keymap.set("n", "<C-h>", require("smart-splits").move_cursor_left)
+vim.keymap.set("n", "<C-j>", require("smart-splits").move_cursor_down)
+vim.keymap.set("n", "<C-k>", require("smart-splits").move_cursor_up)
+vim.keymap.set("n", "<C-l>", require("smart-splits").move_cursor_right)
+vim.keymap.set("n", "<C-\\>", require("smart-splits").move_cursor_previous)
+-- swapping buffers between windows
+vim.keymap.set("n", "<leader><leader>h", require("smart-splits").swap_buf_left)
+vim.keymap.set("n", "<leader><leader>j", require("smart-splits").swap_buf_down)
+vim.keymap.set("n", "<leader><leader>k", require("smart-splits").swap_buf_up)
+vim.keymap.set("n", "<leader><leader>l", require("smart-splits").swap_buf_right)
