3f64745990c7d3a0733e624dddba3e9943998e21
YDH <swimming.polar@gmail.com>
2024-08-12 13:24:58 +0900

    Add .config/nvim/LICENSE Add .config/nvim/README.md Add .config/nvim/.git/HEAD Add .config/nvim/.git/branches/.keep Add .config/nvim/.git/config Add .config/nvim/.git/description Add .config/nvim/.git/hooks/applypatch-msg.sample Add .config/nvim/.git/hooks/commit-msg.sample Add .config/nvim/.git/hooks/fsmonitor-watchman.sample Add .config/nvim/.git/hooks/post-update.sample Add .config/nvim/.git/hooks/pre-applypatch.sample Add .config/nvim/.git/hooks/pre-commit.sample Add .config/nvim/.git/hooks/pre-merge-commit.sample Add .config/nvim/.git/hooks/pre-push.sample Add .config/nvim/.git/hooks/pre-rebase.sample Add .config/nvim/.git/hooks/pre-receive.sample Add .config/nvim/.git/hooks/prepare-commit-msg.sample Add .config/nvim/.git/hooks/push-to-checkout.sample Add .config/nvim/.git/hooks/update.sample Add .config/nvim/.git/index Add .config/nvim/.git/info/exclude Add .config/nvim/.git/logs/HEAD Add .config/nvim/.git/logs/refs/heads/main Add .config/nvim/.git/logs/refs/remotes/origin/HEAD Add .config/nvim/.git/objects/info/.keep Add .config/nvim/.git/objects/pack/pack-86ff5b2920c950d77cf1b5dcab63824903640d0f.idx Add .config/nvim/.git/objects/pack/pack-86ff5b2920c950d77cf1b5dcab63824903640d0f.pack Add .config/nvim/.git/packed-refs Add .config/nvim/.git/refs/heads/main Add .config/nvim/.git/refs/remotes/origin/HEAD Add .config/nvim/.git/refs/tags/.keep Add .config/nvim/.gitignore Add .config/nvim/.neoconf.json Add .config/nvim/init.lua Add .config/nvim/lazy-lock.json Add .config/nvim/lazyvim.json Add .config/nvim/lua/config/autocmds.lua Add .config/nvim/lua/config/keymaps.lua Add .config/nvim/lua/config/lazy.lua Add .config/nvim/lua/config/options.lua Add .config/nvim/lua/plugins/coding/cmp.lua Add .config/nvim/lua/plugins/coding/codeium.lua Add .config/nvim/lua/plugins/coding/console-log.lua Add .config/nvim/lua/plugins/coding/emmet.lua Add .config/nvim/lua/plugins/coding/multicursor.lua Add .config/nvim/lua/plugins/coding/navicbuddy.lua Add .config/nvim/lua/plugins/ui/action-preview.lua Add .config/nvim/lua/plugins/ui/bufferline.lua Add .config/nvim/lua/plugins/ui/color.lua Add .config/nvim/lua/plugins/ui/neo-tree.lua Add .config/nvim/lua/plugins/ui/smart-splits.lua Add .config/nvim/lua/plugins/ui/window-picker.lua Add .config/nvim/lua/plugins/utils/auto-pairs.lua Add .config/nvim/lua/plugins/utils/dial.lua Add .config/nvim/lua/plugins/utils/enhanced-fzf.lua Add .config/nvim/lua/plugins/utils/spider.lua Add .config/nvim/lua/plugins/utils/tailwind-tools.lua Add .config/nvim/lua/plugins/utils/toggleterms.lua Add .config/nvim/stylua.toml

---
+++ dot_config/nvim/lua/plugins/coding/cmp.lua
@@ -0,0 +1,173 @@
+---@diagnostic disable: missing-fields
+local source_mapping = {
+  buffer = "[Buffer]",
+  nvim_lsp = "[LSP]",
+  nvim_lua = "[Lua]",
+  codeium = "[Codeium]",
+  luasnip = "[Snippet]",
+  path = "[Path]",
+  spell = "[Spell]",
+  emoji = "[Emoji]",
+}
+local icons = {
+  Text = " ",
+  Method = "󰆧 ",
+  Function = "󰊕 ",
+  Constructor = " ",
+  Field = "󰇽 ",
+  Variable = "󰂡 ",
+  Class = "󰠱 ",
+  Interface = " ",
+  Module = " ",
+  Property = "󰜢 ",
+  Unit = " ",
+  Value = "󰎠 ",
+  Enum = " ",
+  Keyword = "󰌋 ",
+  Snippet = "󰅱 ",
+  Color = "󰏘 ",
+  File = "󰈙 ",
+  Reference = " ",
+  Folder = "󰉋 ",
+  EnumMember = " ",
+  Constant = "󰏿 ",
+  Struct = " ",
+  Event = " ",
+  Operator = " ",
+  TypeParameter = " ",
+  Codeium = " ",
+  Copilot = " ",
+}
+return {
+  "hrsh7th/nvim-cmp",
+  dependencies = {
+    {
+      "l3mon4d3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-nvim-lua",
+      "hrsh7th/cmp-path",
+      "f3fora/cmp-spell",
+      "lukas-reineke/cmp-under-comparator",
+      "Exafunction/codeium.nvim",
+      "windwp/nvim-autopairs",
+      "onsails/lspkind.nvim",
+    },
+    {
+      "mlaursen/vim-react-snippets",
+      config = function()
+        require("vim-react-snippets").lazy_load()
+      end,
+    },
+  },
+  ---@param opts cmp.ConfigSchema
+  opts = function(_, opts)
+    local cmp = require("cmp")
+    local luasnip = require("luasnip")
+    local compare = require("cmp.config.compare")
+    local lspkind = require("lspkind")
+    -- If you want insert `(` after select function or method item
+    local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+    cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+
+    local has_words_before = function()
+      unpack = unpack or table.unpack
+      local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+      return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+    end
+
+    vim.api.nvim_set_hl(0, "CmpGhostText", {
+      fg = "#808080",
+    })
+    opts.experimental = {
+      ghost_text = { hl_group = "CmpGhostText" },
+    }
+    opts.formatting = {
+      fields = { "abbr", "kind", "menu" },
+      format = function(entry, vim_item)
+        local kind = vim_item.kind
+        local symbol = icons[kind] or lspkind.symbolic(vim_item.kind, { mode = "symbol" })
+        vim_item.kind = " " .. (symbol or "?") .. " " .. kind
+        vim_item.menu = source_mapping[entry.source.name]
+        return vim_item
+      end,
+    }
+    opts.window = {
+      documentation = {
+        border = { "┌", "─", "┐", "│", "┘", "─", "└", "│" },
+        winhighlight = "Normal:CmpPmenu,FloatBorder:CmpPmenuBorder,CursorLine:PmenuSel,Search:None",
+      },
+      completion = {
+        border = { "┌", "─", "┐", "│", "┘", "─", "└", "│" },
+        winhighlight = "Normal:CmpPmenu,FloatBorder:CmpPmenuBorder,CursorLine:PmenuSel,Search:None",
+      },
+    }
+    -- Menu position above/auto
+    opts.view = {
+      entries = { name = "custom", selection_order = "near_cursor", follow_cursor = true },
+    }
+    opts.sources = {
+      {
+        name = "codeium",
+        group_index = 2,
+        max_item_count = 3,
+      },
+      {
+        name = "luasnip",
+        group_index = 2,
+        keyword_length = 2,
+        max_item_count = 3,
+      },
+      {
+        name = "nvim_lsp",
+        group_index = 2,
+        keyword_length = 1,
+        max_item_count = 20,
+      },
+      { name = "spell", group_index = 3, max_item_count = 3 },
+      { name = "buffer", group_index = 3, max_item_count = 3 },
+      { name = "path", group_index = 3, max_item_count = 3 },
+      { name = "emoji", group_index = 3, max_item_count = 3 },
+    }
+    opts.sorting = {
+      priority_weight = 1.0,
+      comparators = {
+        compare.offset,
+        compare.exact,
+        compare.score,
+        compare.recently_used,
+        require("cmp-under-comparator").under,
+        compare.kind,
+        compare.sort_text,
+        compare.length,
+        compare.order,
+      },
+    }
+    opts.performance = {
+      max_view_entries = 20,
+      debounce = 150,
+    }
+    opts.mapping = vim.tbl_extend("force", opts.mapping, {
+      ["<Tab>"] = cmp.mapping(function(fallback)
+        if luasnip.locally_jumpable(1) then
+          luasnip.jump(1)
+        elseif cmp.visible() then
+          cmp.select_next_item()
+        else
+          fallback()
+        end
+      end, { "i", "s" }),
+
+      ["<S-Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_prev_item()
+        elseif luasnip.locally_jumpable(-1) then
+          luasnip.jump(-1)
+        else
+          fallback()
+        end
+      end, { "i", "s" }),
+    })
+  end,
+}
