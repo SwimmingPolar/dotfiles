bc4b4ee2eb045b7b4f1eabde96c83680d45149f6
YDH <swimming.polar@gmail.com>
2024-08-12 13:21:05 +0900

    Remove .config/nvim/LICENSE Remove .config/nvim/README.md Remove .config/nvim/.git/HEAD Remove .config/nvim/.git/branches/.keep Remove .config/nvim/.git/config Remove .config/nvim/.git/description Remove .config/nvim/.git/hooks/applypatch-msg.sample Remove .config/nvim/.git/hooks/commit-msg.sample Remove .config/nvim/.git/hooks/fsmonitor-watchman.sample Remove .config/nvim/.git/hooks/post-update.sample Remove .config/nvim/.git/hooks/pre-applypatch.sample Remove .config/nvim/.git/hooks/pre-commit.sample Remove .config/nvim/.git/hooks/pre-merge-commit.sample Remove .config/nvim/.git/hooks/pre-push.sample Remove .config/nvim/.git/hooks/pre-rebase.sample Remove .config/nvim/.git/hooks/pre-receive.sample Remove .config/nvim/.git/hooks/prepare-commit-msg.sample Remove .config/nvim/.git/hooks/push-to-checkout.sample Remove .config/nvim/.git/hooks/update.sample Remove .config/nvim/.git/index Remove .config/nvim/.git/info/exclude Remove .config/nvim/.git/logs/HEAD Remove .config/nvim/.git/logs/refs/heads/main Remove .config/nvim/.git/logs/refs/remotes/origin/HEAD Remove .config/nvim/.git/objects/info/.keep Remove .config/nvim/.git/objects/pack/pack-86ff5b2920c950d77cf1b5dcab63824903640d0f.idx Remove .config/nvim/.git/objects/pack/pack-86ff5b2920c950d77cf1b5dcab63824903640d0f.pack Remove .config/nvim/.git/packed-refs Remove .config/nvim/.git/refs/heads/main Remove .config/nvim/.git/refs/remotes/origin/HEAD Remove .config/nvim/.git/refs/tags/.keep Remove .config/nvim/.gitignore Remove .config/nvim/.neoconf.json Remove .config/nvim/init.lua Remove .config/nvim/lazy-lock.json Remove .config/nvim/lazyvim.json Remove .config/nvim/lua/config/autocmds.lua Remove .config/nvim/lua/config/keymaps.lua Remove .config/nvim/lua/config/lazy.lua Remove .config/nvim/lua/config/options.lua Remove .config/nvim/lua/plugins/coding/cmp.lua Remove .config/nvim/lua/plugins/coding/console-log.lua Remove .config/nvim/lua/plugins/coding/copilot.lua Remove .config/nvim/lua/plugins/coding/emmet.lua Remove .config/nvim/lua/plugins/coding/multicursor.lua Remove .config/nvim/lua/plugins/ui/action-preview.lua Remove .config/nvim/lua/plugins/ui/bufferline.lua Remove .config/nvim/lua/plugins/ui/color.lua Remove .config/nvim/lua/plugins/ui/neo-tree.lua Remove .config/nvim/lua/plugins/ui/smart-splits.lua Remove .config/nvim/lua/plugins/ui/window-picker.lua Remove .config/nvim/lua/plugins/utils/enhanced-fzf.lua Remove .config/nvim/lua/plugins/utils/tailwind-tools.lua Remove .config/nvim/stylua.toml

--- dot_config/nvim/lua/plugins/ui/action-preview.lua
+++
@@ -1,8 +0,0 @@
--- https://github.com/aznhe21/actions-preview.nvim
--- @TODO: Reconfig keymap and add whichkey
-return {
-  "aznhe21/actions-preview.nvim",
-  config = function()
-    vim.keymap.set({ "v", "n" }, "gf", require("actions-preview").code_actions)
-  end,
-}
