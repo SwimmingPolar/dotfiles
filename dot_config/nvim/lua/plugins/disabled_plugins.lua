8445985a38eb59a320fb259926e673b199516a37
YDH <swimming.polar@gmail.com>
2024-08-16 21:14:06 +0900

    Add .config/nvim/.neoconf.json Add .config/nvim/init.lua Add .config/nvim/lazy-lock.json Add .config/nvim/lazyvim.json Add .config/nvim/lua/config/autocmds.lua Add .config/nvim/lua/config/keymaps.lua Add .config/nvim/lua/config/lazy.lua Add .config/nvim/lua/config/options.lua Add .config/nvim/lua/plugins/coding/cmp.lua Add .config/nvim/lua/plugins/coding/codeium.lua Add .config/nvim/lua/plugins/coding/debugprint.lua Add .config/nvim/lua/plugins/coding/emmet.lua Add .config/nvim/lua/plugins/disabled_plugins.lua Add .config/nvim/lua/plugins/ui/bufferline.lua Add .config/nvim/lua/plugins/ui/color.lua Add .config/nvim/lua/plugins/ui/dashboard.lua Add .config/nvim/lua/plugins/ui/neotree.lua Add .config/nvim/lua/plugins/ui/notify.lua Add .config/nvim/lua/plugins/ui/smart-splits.lua Add .config/nvim/lua/plugins/ui/telescope.lua Add .config/nvim/lua/plugins/ui/window-picker.lua Add .config/nvim/lua/plugins/ui/zenmode.lua Add .config/nvim/lua/plugins/utils/auto-pairs.lua Add .config/nvim/lua/plugins/utils/dial.lua Add .config/nvim/lua/plugins/utils/markdown-preview.lua Add .config/nvim/lua/plugins/utils/multicursor.lua Add .config/nvim/lua/plugins/utils/navbuddy.lua Add .config/nvim/lua/plugins/utils/spider.lua Add .config/nvim/lua/plugins/utils/tailwind-classname.lua Add .config/nvim/lua/plugins/utils/tailwind-tools.lua Add .config/nvim/lua/plugins/utils/text-case.lua Add .config/nvim/stylua.toml

---
+++ dot_config/nvim/lua/plugins/disabled_plugins.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "ThePrimeagen/harpoon",
+    enable = false,
+  },
+  {
+    "echasnovski/mini.pairs",
+    enabled = false,
+  },
+}
---
+++ dot_config/nvim/lua/plugins/ui/notify.lua
@@ -0,0 +1,8 @@
+return {
+  "rcarriga/nvim-notify",
+  opts = {
+    render = "wrapped-compact",
+    stages = "fade_in_slide_out",
+    timeout = 3500,
+  },
+}
