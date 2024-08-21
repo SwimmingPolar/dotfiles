f97d73b6582cc46c0ac318712b5fec3baec5b64f
YDH <swimming.polar@gmail.com>
2024-08-21 10:48:28 +0900

    Remove .zsh/cmds/cd_to_project_root Remove .zsh/cmds/lfcd Remove .zsh/cmds/run_script Remove .zsh/cmds/project_root Remove .zsh/extra Remove .zsh/keybindings/.keep Remove .zsh/scripts/chezmoi_managed Remove .zsh/scripts/install_zinit.sh

--- dot_zsh/scripts/executable_install_zinit.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-bash -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
-echo "Reload and run 'zinit self-update' to update zinit."
