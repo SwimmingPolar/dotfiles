772e174f05a913369a95a073d90154ec73400bfc
YDH <swimming.polar@gmail.com>
2024-08-23 22:22:23 +0900

    Add .config/wezterm/wezterm.lua Add .config/wezterm/wezterm.lua-e

---
+++ dot_config/wezterm/wezterm.lua
@@ -0,0 +1,32 @@
+local wezterm = require("wezterm")
+
+local config = wezterm.config_builder()
+config.default_cwd = "~"
+
+-- Theme
+config.color_scheme = "Gruvbox dark, soft (base16)"
+
+-- Padding
+config.window_padding = {}
+config.window_padding.left = "0.5cell"
+config.window_padding.right = "0.5cell"
+config.window_padding.top = "0.2cell"
+config.window_padding.bottom = "0.2cell"
+
+-- config.tab_bar_at_bottom = true
+config.enable_tab_bar = false
+
+-- Window
+config.initial_rows = 32
+config.initial_cols = 120
+
+-- Window appearance
+config.window_decorations = "RESIZE"
+config.window_background_opacity = 0.925
+
+-- Font
+config.font = wezterm.font("JetBrains Mono NL", { weight = "Bold", italic = false })
+config.font_size = 13
+config.line_height = 1.175
+
+return config
