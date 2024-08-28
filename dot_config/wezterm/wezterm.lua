88ce1cb0e431f5f136d96f9c3135fc95993ac5dc
YDH <swimming.polar@gmail.com>
2024-08-28 11:54:27 +0900

    Add .config/wezterm/wezterm.lua Add .config/wezterm/wezterm.lua-e

---
+++ dot_config/wezterm/wezterm.lua
@@ -0,0 +1,36 @@
+local wezterm = require("wezterm")
+
+local config = wezterm.config_builder()
+config.default_cwd = "~"
+
+-- Theme
+config.color_scheme = "Gruvbox dark, soft (base16)"
+
+-- Padding
+config.window_padding = {}
+config.window_padding.left = 0
+config.window_padding.right = 0
+config.window_padding.top = 0
+config.window_padding.bottom = 0
+
+-- config.tab_bar_at_bottom = true
+config.enable_tab_bar = false
+
+-- Window
+config.initial_rows = 32
+config.initial_cols = 120
+
+-- Window appearance
+config.window_decorations = "RESIZE"
+config.window_background_opacity = 0.925
+
+-- Font
+config.font = wezterm.font("JetBrains Mono NL", { weight = "Bold", italic = false })
+config.font_size = 13
+config.line_height = 1.175
+config.front_end = "OpenGL"
+config.freetype_load_target = "Light"
+config.freetype_render_target = "HorizontalLcd"
+config.cell_width = 0.9
+
+return config
