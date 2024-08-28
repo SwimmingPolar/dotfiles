7599ce2458bf3950a04272819ee2b1b7ac9e42db
YDH <swimming.polar@gmail.com>
2024-08-28 11:54:43 +0900

    Add .config/nvim/.neoconf.json Add .config/nvim/init.lua Add .config/nvim/lazy-lock.json Add .config/nvim/lazyvim.json Add .config/nvim/lua/config/autocmds.lua Add .config/nvim/lua/config/keymaps.lua Add .config/nvim/lua/config/lazy.lua Add .config/nvim/lua/config/options.lua Add .config/nvim/lua/plugins/coding/cmp.lua Add .config/nvim/lua/plugins/coding/codeium.lua Add .config/nvim/lua/plugins/coding/debugprint.lua Add .config/nvim/lua/plugins/coding/emmet.lua Add .config/nvim/lua/plugins/coding/lsp.lua Add .config/nvim/lua/plugins/coding/lua-snip.lua Add .config/nvim/lua/plugins/coding/mason.lua Add .config/nvim/lua/plugins/disabled_plugins.lua Add .config/nvim/lua/plugins/leetcode.lua Add .config/nvim/lua/plugins/ui/bufferline.lua Add .config/nvim/lua/plugins/ui/color.lua Add .config/nvim/lua/plugins/ui/dashboard.lua Add .config/nvim/lua/plugins/ui/lsp.lua Add .config/nvim/lua/plugins/ui/neotree.lua Add .config/nvim/lua/plugins/ui/noice.lua Add .config/nvim/lua/plugins/ui/notify.lua Add .config/nvim/lua/plugins/ui/smart-splits.lua Add .config/nvim/lua/plugins/ui/telescope.lua Add .config/nvim/lua/plugins/ui/twilight.lua Add .config/nvim/lua/plugins/ui/whichkey.lua Add .config/nvim/lua/plugins/ui/window-picker.lua Add .config/nvim/lua/plugins/ui/zenmode.lua Add .config/nvim/lua/plugins/utils/auto-pairs.lua Add .config/nvim/lua/plugins/utils/dial.lua Add .config/nvim/lua/plugins/utils/harpoon.lua Add .config/nvim/lua/plugins/utils/markdown-preview.lua Add .config/nvim/lua/plugins/utils/multicursor.lua Add .config/nvim/lua/plugins/utils/navbuddy.lua Add .config/nvim/lua/plugins/utils/resession.lua Add .config/nvim/lua/plugins/utils/tabout.lua Add .config/nvim/lua/plugins/utils/tailwind-tools.lua Add .config/nvim/lua/plugins/utils/text-case.lua Add .config/nvim/stylua.toml

---
+++ dot_config/nvim/lua/plugins/coding/mason.lua
@@ -0,0 +1,12 @@
+return {
+  "williamboman/mason.nvim",
+  opts = {
+    ui = {
+      icons = {
+        package_pending = " ",
+        package_installed = " ",
+        package_uninstalled = " ",
+      },
+    },
+  },
+}
