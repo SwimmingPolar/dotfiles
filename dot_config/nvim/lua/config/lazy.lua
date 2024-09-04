1675e117f917d395c5b901cd65b3819e4da62de8
YDH <swimming.polar@gmail.com>
2024-09-05 08:47:17 +0900

    Add .config/nvim/README.md Add .config/nvim/.neoconf.json Add .config/nvim/init.lua Add .config/nvim/lazy-lock.json Add .config/nvim/lazyvim.json Add .config/nvim/lua/config/autocmds.lua Add .config/nvim/lua/config/highlights.lua Add .config/nvim/lua/config/keymaps.lua Add .config/nvim/lua/config/lazy.lua Add .config/nvim/lua/config/options.lua Add .config/nvim/lua/plugins/coding/cmp.lua Add .config/nvim/lua/plugins/coding/codeium.lua Add .config/nvim/lua/plugins/coding/debugprint.lua Add .config/nvim/lua/plugins/coding/emmet.lua Add .config/nvim/lua/plugins/coding/lsp.lua Add .config/nvim/lua/plugins/coding/lua-snip.lua Add .config/nvim/lua/plugins/coding/mason.lua Add .config/nvim/lua/plugins/custom/custom-copilot.lua Add .config/nvim/lua/plugins/disabled_plugins.lua Add .config/nvim/lua/plugins/leetcode.lua Add .config/nvim/lua/plugins/ui/bufferline.lua Add .config/nvim/lua/plugins/ui/colors.lua Add .config/nvim/lua/plugins/ui/dashboard.lua Add .config/nvim/lua/plugins/ui/icons.lua Add .config/nvim/lua/plugins/ui/lsp.lua Add .config/nvim/lua/plugins/ui/neotree.lua Add .config/nvim/lua/plugins/ui/noice.lua Add .config/nvim/lua/plugins/ui/notify.lua Add .config/nvim/lua/plugins/ui/smart-splits.lua Add .config/nvim/lua/plugins/ui/telescope.lua Add .config/nvim/lua/plugins/ui/twilight.lua Add .config/nvim/lua/plugins/ui/whichkey.lua Add .config/nvim/lua/plugins/ui/window-picker.lua Add .config/nvim/lua/plugins/ui/zenmode.lua Add .config/nvim/lua/plugins/utils/auto-pairs.lua Add .config/nvim/lua/plugins/utils/dial.lua Add .config/nvim/lua/plugins/utils/harpoon.lua Add .config/nvim/lua/plugins/utils/markdown-preview.lua Add .config/nvim/lua/plugins/utils/multicursor.lua Add .config/nvim/lua/plugins/utils/navbuddy.lua Add .config/nvim/lua/plugins/utils/resession.lua Add .config/nvim/lua/plugins/utils/tabout.lua Add .config/nvim/lua/plugins/utils/tailwind-tools.lua Add .config/nvim/lua/plugins/utils/text-case.lua Add .config/nvim/lua/plugins/utils/vim-tmux.lua Add .config/nvim/stylua.toml

---
+++ dot_config/nvim/lua/config/lazy.lua
@@ -0,0 +1,49 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable",
+        lazypath,
+    })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+    spec = {
+        { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+        { import = "plugins.coding" },
+        { import = "plugins.ui" },
+        { import = "plugins.utils" },
+        { import = "plugins.disabled_plugins" },
+        { import = "plugins.leetcode" },
+        { import = "plugins.custom" },
+    },
+    defaults = {
+        lazy = false,
+        version = false, -- always use the latest git commit
+    },
+    install = { colorscheme = { "catppucin", "gruvbox-material" } },
+    checker = { enabled = true, notify = false }, -- automatically check for plugin updates
+    ui = {
+        icons = {
+            ft = " ",
+            lazy = "󰂠  ",
+            loaded = " ",
+            not_loaded = " ",
+        },
+        border = "rounded",
+        backdrop = 70,
+    },
+    performance = {
+        rtp = {
+            disabled_plugins = {},
+        },
+    },
+})
+
+-- load highlights
+require("config.highlights")
---
+++ dot_config/nvim/lua/plugins/coding/lsp.lua
@@ -0,0 +1,3 @@
+local M = {}
+-- require("lspconfig").astro.setup({})
+return M
