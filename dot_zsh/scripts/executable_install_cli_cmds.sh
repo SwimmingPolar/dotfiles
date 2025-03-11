48165777a0df9436d073d70cde4a0a7de8fce1af
YDH <swimming.polar@gmail.com>
2025-03-11 14:21:50 +0900

    zsh

--- dot_zsh/scripts/executable_install_cli_cmds.sh
+++ dot_zsh/scripts/executable_install_cli_cmds.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
 
 cli_cmds=(
-    eza
-    chezmoi
-    lazygit
-    neovim
-    bpytop
-    tlrc
-    lf
-    bat
+  eza
+  chezmoi
+  lazygit
+  neovim
+  bpytop
+  tldr
+  lf
+  bat
+  tokei
 )
 
 install_cmds() {
-    for cmd in "${cli_cmds[@]}"; do
-        if ! command -v "$cmd" &>/dev/null; then
-            echo "$(green Installing:) $cmd..."
-            brew install "$cmd"
-        fi
-    done
+  for cmd in "${cli_cmds[@]}"; do
+    if ! command -v "$cmd" &>/dev/null; then
+      echo "$(green Installing:) $cmd..."
+      brew install "$cmd"
+    fi
+  done
 }
 # Check if 'brew' is installed
 if ! command -v brew &>/dev/null; then
-    echo "$(yellow Warning:) Homebrew is not installed."
-    echo "$(yellow Install Homebrew and restart the shell.)"
+  echo "$(yellow Warning:) Homebrew is not installed."
+  echo "$(yellow Install Homebrew and restart the shell.)"
 else
-    install_cmds
+  install_cmds
 fi
